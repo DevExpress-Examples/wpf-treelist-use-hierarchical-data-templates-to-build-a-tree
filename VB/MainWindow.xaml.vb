4838d51b9338ad2cf5d343827d17ff8c50b3bc87
DevExpressExampleBot <fakeEmail>
2022-10-04 15:00:25 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,125 +1,12 @@
-﻿Imports System
 Imports System.Windows
-Imports System.Windows.Data
-Imports System.Windows.Markup
 
 Namespace HierarchicalDataTemplateMode
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            DataContext = New ViewModel()
+            Me.InitializeComponent()
         End Sub
-    End Class
-
-    Public Class ProjectObjectToProgressConverter
-        Inherits MarkupExtension
-        Implements IValueConverter
-
-        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-            Dim po = TryCast(value, ProjectObject)
-            If po IsNot Nothing Then
-                Return po.Progress
-            End If
-            Return Binding.DoNothing
-         '   return value;
-        End Function
-
-        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-            Return value
-        End Function
-
-        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-            Return Me
-        End Function
-
-
-    End Class
-    Public Class ProjectStageToProgressConverter
-        Inherits MarkupExtension
-        Implements IValueConverter
-
-        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-            Dim ps = TryCast(value, ProjectObject)
-            If ps IsNot Nothing Then
-                Return ps.Progress
-            End If
-            Return Binding.DoNothing
-            '   return value;
-        End Function
-
-        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-            Return value
-        End Function
-
-        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-            Return Me
-        End Function
-    End Class
-
-    Public Class TaskToStartDateConverter
-        Inherits MarkupExtension
-        Implements IValueConverter
-
-        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-            Dim obj = TryCast(value, Task)
-            If obj IsNot Nothing Then
-                Return obj.StartDate
-            End If
-            Return Binding.DoNothing
-            '   return value;
-        End Function
-
-        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-            Return value
-        End Function
-
-        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-            Return Me
-        End Function
-    End Class
-    Public Class TaskToEndDateConverter
-        Inherits MarkupExtension
-        Implements IValueConverter
-
-        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-            Dim obj = TryCast(value, Task)
-            If obj IsNot Nothing Then
-                Return obj.EndDate
-            End If
-            Return Binding.DoNothing
-            '   return value;
-        End Function
-
-        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-            Return value
-        End Function
-
-        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-            Return Me
-        End Function
-    End Class
-    Public Class TaskToStateConverter
-        Inherits MarkupExtension
-        Implements IValueConverter
-
-        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-            Dim obj = TryCast(value, Task)
-            If obj IsNot Nothing Then
-                Return obj.State
-            End If
-            Return Binding.DoNothing
-            '   return value;
-        End Function
-
-        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-            Return value
-        End Function
-
-        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-            Return Me
-        End Function
     End Class
 End Namespace
